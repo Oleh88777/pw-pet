c13b674fb38e7d82a5298b4cc9185f0603fcd9b6
Oleh <mykhayliv88777@gmail.com>
2026-08-18 20:06:55 +0200

    added api class and auth test

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,6 +1,8 @@
+
 import {test as setup, expect} from '@playwright/test';
 import {manualTestUser} from "../test-data/testUser";
 import {LoginPage} from "../pages/login";
+import {ApiAuth} from "../api-clas/authApi";
 
 
 const authFile = 'playwright/.auth/user.json';
@@ -8,29 +10,9 @@ const authFile = 'playwright/.auth/user.json';
 setup('authenticate', async ({page, request}) => {
 
     const loginPage = new LoginPage(page);
+    const apiAuth = new ApiAuth(request);
 
-    const apiRegisterNewUser = await request.post('https://api.practicesoftwaretesting.com/users/register', {
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-            'Accept': 'application/json',
-        },
-        data: {
-            first_name: manualTestUser.firstName,
-            last_name: manualTestUser.lastName,
-            email: manualTestUser.emailAddress,
-            password: manualTestUser.password,
-            dob: manualTestUser.dateOfBirth,
-            phone: manualTestUser.phone,
-            address: {
-                street: manualTestUser.street,
-                house_number: manualTestUser.houseNumber,
-                city: manualTestUser.city,
-                state: manualTestUser.state,
-                country: manualTestUser.country,
-                postal_code: manualTestUser.postalCode,
-            },
-        },
-    })
+    const apiRegisterNewUser = await apiAuth.manualRegister(manualTestUser);
 
     const apiStatus = apiRegisterNewUser.status();
     expect([201, 409]).toContain(apiStatus);
